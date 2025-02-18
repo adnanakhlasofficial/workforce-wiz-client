9e8d8911676a0047263210d22f68ab6a1d1468ba
adnanakhlasofficial <adnan.b.akhlas.official@gmail.com>
2025-02-18 16:29:12 +0600

    navbar sticky done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/shared/Navbar/Navbar";
-import Footer from "../components/shared/Footer/Footer";
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/shared/Navbar/Navbar';
+import Footer from '../components/shared/Footer/Footer';
 
 const MainLayout = () => {
   return (
     <div>
-      <header className="bg-errigalWhite shadow-lg">
+      <header className='bg-errigalWhite/70 backdrop-blur-lg shadow-lg sticky top-0 z-10'>
         {/* navbar */}
         <Navbar />
       </header>
-      <main className="min-h-[calc(100vh-14.5rem)]">
+      <main className='min-h-[calc(100vh-14.5rem)]'>
         <Outlet />
       </main>
-      <footer className="bg-errigalWhite min-h-44">
+      <footer className='bg-errigalWhite min-h-44'>
         {/* footer */}
         <Footer />
       </footer>
